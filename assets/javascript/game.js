911a76aaedcdb321a9d3b3965dc26589bb6e89a5
Srdjan Markovich <srdjan01@msn.com>
2017-11-09 14:32:32 -0700

    Added new file

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// Guess what letter I'm thinking of 
+// 1.Assign a random letter to a variable
+// 2.Capture a user input
+// 3.Compere it with random letter
+// 4.Checking if is match or if is not
+// 5.If is match tell user ("You Won") and display +1 to wins and geusses are reseted to 9 
+// 6.If is not a match display (1.guesses left: -1)(Your guesses so far: add guessed letter..etc h)
+// 7.If guesses left are = 0 then losses +1; guesses resets to 9.
+
+
+// Wins: (# of times the user has guessed the letter correctly)
+// Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+// Guesses Left: (# of guesses left. This will update)
+// Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
+// When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+// When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins
